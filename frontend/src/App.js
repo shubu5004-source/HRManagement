a63decacacdba793560b8ca77749efcdca148ff9
GitHub User <noreply@github.com>
2026-02-20 10:43:26 +0530

    remove the Admindashbord  and when admin login it will redirect to post notification

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,105 +1,8 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-// import "./App.css";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
-// import AdminDashboard from "./pages/AdminDashboard";
-// import UserDashboard from "./pages/UserDashboard";
-// import ProtectedRoute from "./auth/ProtectedRoute";
-// import AdminUserSearch from "./pages/AdminUserSearch";
-// import UserProfile from "./pages/UserProfile";
-// import AdminUserList from "./pages/AdminUserList";
-// import AdminEditUserPage from "./pages/AdminEditUserPage";
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Navbar />
-
-//       <div className="main-content">
-//         <Routes>
-//           <Route path="/" element={<Login />} />
-
-//           <Route
-//             path="/register"
-//             element={
-//               <ProtectedRoute role="Admin">
-//                 <Register />
-//               </ProtectedRoute>
-//             }
-//           />
-
-//           <Route
-//             path="/admin"
-//             element={
-//               <ProtectedRoute role="Admin">
-//                 <AdminDashboard />
-//               </ProtectedRoute>
-//             }
-//           />
-//             <Route
-//             path="/admin/edit/:id"
-//             element={
-//               <ProtectedRoute role="Admin">
-//                 <AdminEditUserPage/>
-//               </ProtectedRoute>
-//             }
-//           />
-
-
-//           <Route
-//             path="/admin/all-users"
-//             element={
-//               <ProtectedRoute role="Admin">
-//                 <AdminUserList />
-//               </ProtectedRoute>
-//             }
-//           />
-
-//           <Route
-//             path="/admin/users"
-//             element={
-//               <ProtectedRoute role="Admin">
-//                 <AdminUserSearch />
-//               </ProtectedRoute>
-//             }
-//           />
-
-//           <Route
-//             path="/profile"
-//             element={
-//               <ProtectedRoute role="User">
-//                 <UserProfile />
-//               </ProtectedRoute>
-//             }
-//           />
-
-//           <Route
-//             path="/user"
-//             element={
-//               <ProtectedRoute role="User">
-//                 <UserDashboard />
-//               </ProtectedRoute>
-//             }
-//           />
-//         </Routes>
-//       </div>
-//     </BrowserRouter>
-//   );
-// }
-
-
-
-// export default App;
-
-
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import AdminDashboard from "./pages/AdminDashboard";
 import UserDashboard from "./pages/UserDashboard";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import AdminUserSearch from "./pages/AdminUserSearch";
@@ -128,15 +31,6 @@ function AppContent() {
             element={
               <ProtectedRoute role="Admin">
                 <Register />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/admin"
-            element={
-              <ProtectedRoute role="Admin">
-                <AdminDashboard />
               </ProtectedRoute>
             }
           />
